62f4610b385c5efbe0d71e1117809fa10ae8bc17
LMO <lam@cphbusiness.dk>
2014-03-27 20:20:52 +0100

    Initial Commit

---
+++ src/gitbranch/GitBranch.java
@@ -0,0 +1,16 @@
+package gitbranch;
+
+/**
+ *
+ * @author plaul1
+ */
+public class GitBranch {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
